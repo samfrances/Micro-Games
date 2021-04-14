bab577ce030a9eeec693f9afbc7a0930277c3166
Sam Frances <sam@samfrances.uk>
2021-04-14 08:52:56 +0100

    Indentation

--- Computer-SpaceGames/Starship-Takeoff/cpp/src/main.cpp
+++ Computer-SpaceGames/Starship-Takeoff/cpp/src/main.cpp
@@ -4,43 +4,43 @@
 
 int main(int argc, char const *argv[])
 {
-  std::random_device rd;
-  std::mt19937_64 eng(rd());
+    std::random_device rd;
+    std::mt19937_64 eng(rd());
 
-  std::uniform_int_distribution<unsigned int> gravity_distr{1, 20};
-  std::uniform_int_distribution<unsigned int> weight_distr{1, 40};
+    std::uniform_int_distribution<unsigned int> gravity_distr{1, 20};
+    std::uniform_int_distribution<unsigned int> weight_distr{1, 40};
 
-  auto gravity =  gravity_distr(eng);
-  auto weight =  weight_distr(eng);
+    auto gravity =  gravity_distr(eng);
+    auto weight =  weight_distr(eng);
 
-  std::cout << gravity << std::endl;
-  std::cout << weight << std::endl;
+    std::cout << gravity << std::endl;
+    std::cout << weight << std::endl;
 
-  SpaceTakeoffGame game(gravity, weight);
+    SpaceTakeoffGame game(gravity, weight);
 
-  std::cout << "STARSHIP TAKE-OFF" << std::endl;
-  std::cout << "GRAVITY=" << gravity << std::endl;
-  std::cout << "TYPE IN FORCE" << std::endl;
-  while (true) {
-    unsigned int guess;  
-    std::cin >> guess;
-    switch(game.make_guess(guess)) {
-      case GuessResponse::TooHigh: {
-        std::cout << "TOO HIGH, TRY AGAIN" << std::endl;
-        break;
-      }
-      case GuessResponse::TooLow: {
-        std::cout << "TOO LOW, TRY AGAIN" << std::endl;
-        break;
-      }
-      case GuessResponse::TakeOff: {
-        std::cout << "GOOD TAKE OFF" << std::endl;
-        return 0;
-      }
-      case GuessResponse::GameOver: {
-        std::cout << "YOU FAILED - THE ALIENS GOT YOU" << std::endl;
-        return 0;
-      }
-    } 
-  }
+    std::cout << "STARSHIP TAKE-OFF" << std::endl;
+    std::cout << "GRAVITY=" << gravity << std::endl;
+    std::cout << "TYPE IN FORCE" << std::endl;
+    while (true) {
+        unsigned int guess;  
+        std::cin >> guess;
+        switch(game.make_guess(guess)) {
+            case GuessResponse::TooHigh: {
+                std::cout << "TOO HIGH, TRY AGAIN" << std::endl;
+                break;
+            }
+            case GuessResponse::TooLow: {
+                std::cout << "TOO LOW, TRY AGAIN" << std::endl;
+                break;
+            }
+            case GuessResponse::TakeOff: {
+                std::cout << "GOOD TAKE OFF" << std::endl;
+                return 0;
+            }
+            case GuessResponse::GameOver: {
+                std::cout << "YOU FAILED - THE ALIENS GOT YOU" << std::endl;
+                return 0;
+            }
+        } 
+    }
 }
--- Computer-SpaceGames/Starship-Takeoff/cpp/src/test.cpp
+++ Computer-SpaceGames/Starship-Takeoff/cpp/src/test.cpp
@@ -2,84 +2,84 @@
 #include "game.h"
 
 TEST(GameTest, Initialization) {
-  SpaceTakeoffGame{5, 10};
+    SpaceTakeoffGame{5, 10};
 }
 
 TEST(GameTest, TooHigh) {
-  SpaceTakeoffGame game{5, 10};
-  EXPECT_EQ(
-    game.make_guess(100),
-    GuessResponse::TooHigh
-  );
+    SpaceTakeoffGame game{5, 10};
+    EXPECT_EQ(
+        game.make_guess(100),
+        GuessResponse::TooHigh
+    );
 }
 
 TEST(GameTest, OnlyJustTooHigh) {
-  SpaceTakeoffGame game{5, 10};
-  EXPECT_EQ(
-    game.make_guess(51),
-    GuessResponse::TooHigh
-  );
+    SpaceTakeoffGame game{5, 10};
+    EXPECT_EQ(
+        game.make_guess(51),
+        GuessResponse::TooHigh
+    );
 }
 
 TEST(GameTest, TooLow) {
-  SpaceTakeoffGame game{5, 10};
-  EXPECT_EQ(
-    game.make_guess(4),
-    GuessResponse::TooLow
-  );
+    SpaceTakeoffGame game{5, 10};
+    EXPECT_EQ(
+        game.make_guess(4),
+        GuessResponse::TooLow
+    );
 }
 
 TEST(GameTest, OnlyJustTooLow) {
-  SpaceTakeoffGame game{5, 10};
-  EXPECT_EQ(
-    game.make_guess(49),
-    GuessResponse::TooLow
-  );
+    SpaceTakeoffGame game{5, 10};
+    EXPECT_EQ(
+        game.make_guess(49),
+        GuessResponse::TooLow
+    );
 }
 
 TEST(GameTest, TakeOff) {
-  SpaceTakeoffGame game{5, 10};
-  EXPECT_EQ(
-    game.make_guess(50),
-    GuessResponse::TakeOff
-  );
+    SpaceTakeoffGame game{5, 10};
+    EXPECT_EQ(
+        game.make_guess(50),
+        GuessResponse::TakeOff
+    );
 }
 
 TEST(GameTest, GameOver) {
-  SpaceTakeoffGame game{5, 10};
+    SpaceTakeoffGame game{5, 10};
 
-  for (int i = 0; i < 10; i++) {
-    game.make_guess(100);
-  }
+    for (int i = 0; i < 10; i++) {
+        game.make_guess(100);
+    }
 
-  EXPECT_EQ(
-    game.make_guess(51),
-    GuessResponse::GameOver
-  );
+    EXPECT_EQ(
+        game.make_guess(51),
+        GuessResponse::GameOver
+    );
 }
 
 TEST(GameTest, GameNotQuiteOver) {
-  SpaceTakeoffGame game{5, 10};
+    SpaceTakeoffGame game{5, 10};
 
-  for (int i = 0; i < 9; i++) {
-    game.make_guess(100);
-  }
+    for (int i = 0; i < 9; i++) {
+        game.make_guess(100);
+    }
 
-  EXPECT_NE(
-    game.make_guess(50),
-    GuessResponse::GameOver
-  );
+    EXPECT_NE(
+        game.make_guess(50),
+        GuessResponse::GameOver
+    );
 }
 
 TEST(GameTest, GameVeryMuchOver) {
-  SpaceTakeoffGame game{5, 10};
+    SpaceTakeoffGame game{5, 10};
 
-  for (int i = 0; i < 100; i++) {
-    game.make_guess(100);
-  }
+    for (int i = 0; i < 100; i++) {
+        game.make_guess(100);
+    }
 
-  EXPECT_EQ(
-    game.make_guess(51),
-    GuessResponse::GameOver
-  );
+    EXPECT_EQ(
+        game.make_guess(51),
+        GuessResponse::GameOver
+    );
 }
